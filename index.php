fa89597d89943df557b9d0eb2d6445e51d8fcb55
rubC0d3r <rubenjochemsen@gmail.com>
2022-02-10 17:34:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang=nl-NL>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<header>
+<title>je mama</title>
+<link rel="stylesheet" href="style.css">
+
+</header>
+<body>
+  <h1>JE MAMA IS NIET HALLAL</h1>
+  <p>gisteren is door de FBI geconstateerd dat je mama niet hallal is. Verdere informatie volgt nog</p>
+</body>
