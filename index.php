f9e46070f56515de8a019a2b0ef4f985371a6ba9
rubC0d3r <rubenjochemsen@gmail.com>
2022-02-10 17:42:40 +0100

    het cssbestand

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <header>
 <title>je mama</title>
+<!-- HIER MOETEN WE DE CSS IN DOEN:-->
 <link rel="stylesheet" href="style.css">
 
 </header>
